3d921ed68c020ece36485612a97631f75c9b28c8
AidanP <aidannpratt@gmail.com>
2021-06-07 23:18:15 -0400

    eh

--- src/main/java/net/jimthescientist/mushroomfinder/Main.java
+++ src/main/java/net/jimthescientist/mushroomfinder/Main.java
@@ -9,17 +9,11 @@ import kaptainwutax.terrainutils.TerrainGenerator;
 public class Main {
     public static void main(String[] args){
         long worldSeed = 12L;
-        MCVersion version = MCVersion.v1_16_4;
+        MCVersion version = MCVersion.v1_16_5;
         Dimension dimension = Dimension.NETHER;
         BiomeSource biomeSource = BiomeSource.of(dimension, version, worldSeed);
-        TerrainGenerator terrainGenerator = TerrainGenerator.of(biomeSource);
 
-        assert terrainGenerator != null;
-        Block[] column = terrainGenerator.getColumnAt(0, 0);
-        int x = 0;
-        for (Block blockClass : column) {
-            System.out.println(blockClass + ", Y Coordinate: " + x);
-            x++;
-        }
+
+
     }
 }
