373c8f8c6a4199e81cadc51163d49df2a4d1c23a
AidanP <aidannpratt@gmail.com>
2021-06-12 05:36:59 -0400

    i guess it doesn't do mushrooms, but whatever

--- src/main/java/net/jimthescientist/mushroomfinder/Main.java
+++ src/main/java/net/jimthescientist/mushroomfinder/Main.java
@@ -8,12 +8,54 @@ import kaptainwutax.terrainutils.TerrainGenerator;
 
 public class Main {
     public static void main(String[] args){
-        long worldSeed = 12L;
+        long worldSeed = 4312L;
         MCVersion version = MCVersion.v1_16_5;
-        Dimension dimension = Dimension.NETHER;
+        Dimension dimension = Dimension.OVERWORLD;
         BiomeSource biomeSource = BiomeSource.of(dimension, version, worldSeed);
+        TerrainGenerator terrainGenerator = TerrainGenerator.of(biomeSource);
+        assert terrainGenerator != null;
+        System.out.print("Calculations Initiated");
+        int matchesFound = 0;
+        for (int x = -2000; x <=2000;x++){
+            for (int z = -2000; z <=2000;z++){
+                int originalBlockInt = terrainGenerator.getHeightOnGround(x,z);
+                if (originalBlockInt == 67){
+                    //System.out.println("First Check Passed: "+ x + ", " + z);
+                    if (terrainGenerator.getHeightOnGround(x,z-1) == 68){
+                        //System.out.println("Second Check Passed: "+ x + ", " + z);
+                        if (terrainGenerator.getHeightOnGround(x,z-2) == 68){
+                            //System.out.println("Third Check Passed: "+ x + ", " + z);
+                            if (terrainGenerator.getHeightOnGround(x,z-3) == 69){
+                                //System.out.println("Fourth Check Passed: "+ x + ", " + z);
+                                if (terrainGenerator.getHeightOnGround(x+1,z-3) == 66){
+                                    //System.out.println("Fifth Check Passed: "+ x + ", " + z);
+                                    if (terrainGenerator.getHeightOnGround(x+1,z-2) == 66){
+                                        //System.out.println("Sixth Check Passed: "+ x + ", " + z);
+                                        if (terrainGenerator.getHeightOnGround(x+1,z-1) == 65){
+                                            //System.out.println("Seventh Check Passed: "+ x + ", " + z);
+                                            if (terrainGenerator.getHeightOnGround(x+1,z) == 65){
+                                                //System.out.println("Eighth Check Passed: "+ x + ", " + z);
+                                                if (terrainGenerator.getHeightOnGround(x+2,z) == 64){
+                                                    //System.out.println("Ninth Check Passed: "+ x + ", " + z);
+                                                    if (terrainGenerator.getHeightOnGround(x+2,z-1) == 64){
+                                                        //System.out.println("Tenth Check Passed: "+ x + ", " + z);
+                                                        if (terrainGenerator.getHeightOnGround(x+2,z-2) == 64){
+                                                            System.out.println("Eleventh Check Passed: "+ x + ", " + z);
+                                                            matchesFound +=1;
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
 
-
-
+            }
+        }
+        System.out.print("Calculations Complete, "+ matchesFound+ " matches found");
     }
 }
